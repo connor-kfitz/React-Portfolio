98d788f44e67b92a3c1b49f6d9a87e8a4c9319a5
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-21 14:45:34 -0400

    First Push, Created App

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <div id="nav" class="flex">
+                <ul>
+                    <li><a href="" class="navItem">About</a></li>
+                    <li><a href="" class="navItem">Projects</a></li>
+                    <li><a href="" class="navItem">Resume</a></li>
+                </ul>
+                <a href="">Contact</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
