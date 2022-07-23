f0ee25a0c2157be17234a801c54dc824a139fa96
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-23 17:24:27 -0400

    Added Resume Download Link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../../styles/Resume.css";
-
+import resume from "../../downloadable-files/Resume-Connor-Fitzsimmons.docx"
 
 function Resume() {
     return (
         <main>
-
+            <a href={resume} download>Click to Download</a>
         </main>
     )
 }
