8a29ca51dba8bb0a0e1bb6ff1a3d94bef82ddbae
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-23 16:27:06 -0400

    Added Contact Info Verificaiton

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from 'emailjs-com';
+import { validateEmail } from "../../utils/helpers";
 import "../../styles/Contact.css";
 
 
@@ -7,7 +8,7 @@ function Contact() {
     const form = useRef();
 
     const sendEmail = (e) => {
-      e.preventDefault();
+    //   e.preventDefault();
   
       emailjs.sendForm('service_hftbn2b', 'template_wpexs8b', form.current, 'XKCZNIV3S0zU37FTp')
         .then((result) => {
@@ -15,27 +16,66 @@ function Contact() {
         }, (error) => {
             console.log(error.text);
         });
-        e.target.reset()
+        // e.target.reset()
+    };
+
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'user_email') {
+          setEmail(inputValue);
+        } else if (inputType === 'user_name') {
+          setUserName(inputValue);
+        }
+      };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!userName){
+            setErrorMessage('Please enter a name!');
+            return;
+        } else if(!validateEmail(email)) {
+            setErrorMessage('Please enter a valid email');
+            return;
+        }
+
+        sendEmail();
+        setUserName('');
+        setEmail('');
     };
   
     return (
     <main>
         <h1>Contact Form</h1>
-        <form ref={form} onSubmit={sendEmail}>
+        <form ref={form}>
             <div className="formItem">
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="user_name" onChange={handleInputChange}/>
             </div>
             <div className="formItem">
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input type="email" name="user_email" onChange={handleInputChange}/>
             </div>
             <div className="formItem">
                 <label>Message</label>
-                <textarea name="message" />
+                <textarea name="message"/>
             </div>
-        <input id="formSubmit" type="submit" value="Send" />
+        <input id="formSubmit" type="submit" value="Send" onClick={handleFormSubmit} />
         </form>
+        {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </main>
     );
 }
