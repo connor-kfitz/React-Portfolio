c33cfbc3b60bb57bd3cdedfc2ee8c78a839155ed
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-24 12:41:04 -0400

    Finish Projects Page

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,7 +14,8 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">weatherApp;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">frontEnd</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">false;</span></h4>
-                            <h4 className="yellow">&#125;</h4> 
+                            <h4 className="yellow aboveDep">&#125;</h4> 
+                            <a className="deployedLink" href="https://connor-kfitz.github.io/purple-orangutan/" target="blank">Deployed Application Link</a>
                         </div> 
                     </a>
 
@@ -24,7 +25,8 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">breweryNearMe;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">frontEnd</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">true;</span></h4>
-                            <h4 className="yellow">&#125;</h4>
+                            <h4 className="yellow aboveDep">&#125;</h4>
+                            <a className="deployedLink" href="https://tyomoto.github.io/potential-meme/" target="blank">Deployed Application Link</a>
                         </div>
                     </a>
 
@@ -34,7 +36,8 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">teamProfileGenerator;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">backEnd</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">false;</span></h4>
-                            <h4 className="yellow">&#125;</h4>
+                            <h4 className="yellow aboveDep">&#125;</h4>
+                            <a className="deployedLink" href="https://drive.google.com/file/d/1l1xIeEsJvLpnWYvqGxA7xMza6YuagLu2/view" target="blank">Deployed Application Link</a>
                         </div>
                     </a>
 
@@ -44,13 +47,10 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">techBlog;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">fullStack</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">false;</span></h4>
-                            <h4 className="yellow">&#125;</h4>
+                            <h4 className="yellow aboveDep">&#125;</h4>
+                            <a className="deployedLink" href="https://vast-shore-46783.herokuapp.com/" target="blank">Deployed Application Link</a>
                         </div>
                     </a>
-                    <div>
-                        <a className="deployedLink" href="https://vast-shore-46783.herokuapp.com/" target="blank">Deployed Application Link</a>
-                    </div>
-
 
                     <a href="https://github.com/connor-kfitz/purple-frog" target="blank" className ="projectAnchor">
                     <div id="projectFive" className="projectCard">  
@@ -58,7 +58,9 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">reactPortfolio;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">frontEnd</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">false;</span></h4>
-                            <h4 className="yellow">&#125;</h4>
+                            <h4 className="yellow aboveDep">&#125;</h4>
+                            <a className="deployedLink" href="" target="blank">Deployed Application Link</a>
+
                         </div>
                     </a>
 
@@ -68,7 +70,9 @@ function Project() {
                             <h4 className="projectIndent">name: <span className="lightGreen">projectThreePlaceHolder;</span></h4>
                             <h4 className="projectIndent">type: <span className="lightGreen">fullStackMERN</span></h4>
                             <h4 className="projectIndent">collaboration: <span className="blue">true;</span></h4>
-                            <h4 className="yellow">&#125;</h4>
+                            <h4 className="yellow aboveDep">&#125;</h4>
+                            <a className="deployedLink" href="" target="blank">Deployed Application Link</a>
+
                         </div>
                     </a>
 
--- src/components/pages/Stack.js
+++ src/components/pages/Stack.js
@@ -18,67 +18,59 @@ import { motion } from 'framer-motion'
 function Stack() {
     return (
     <div>
+
         <div id="main">
-                <h1 className="stackTitle">MERN</h1>
-                <div className="stackIconOuterContainer flex">
-                    
-                    <div className="flex stackIconSingleContainer">
-                        <h4>React</h4>
-                        <motion.img src={reactIcon} alt="React Icon" className="iconImage" whileHover={{ rotate: 360, transition: { repeat: Infinity, repeatType: 'loop', duration: 5 } }}/>
-                    </div>
 
-                    <div className="flex stackIconSingleContainer">
-                        <h4>Node.Js</h4>
-                        <motion.img src={nodeIcon} alt="Node.Js Icon" className="iconImage" whileHover={{ rotate: [0, 30, 90, 150, 210, 270, 330, 360], transition: { repeat: Infinity, repeatType: 'loop', duration: 6 } }}/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer">
-                        <h4>MongoDB</h4>
-                        <img src={mongoIcon} alt="MongoDB Icon" className="iconImage"/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer lastItemAlign">
-                        <h4>Express.Js</h4>
-                        <img src={expressIcon} alt="Express.Js Icon" className="iconImage"/>
-                    </div>
+            <h1 className="stackTitle">MERN</h1>
+            <div className="stackIconOuterContainer flex"> 
+                <div className="flex stackIconSingleContainer">
+                    <h4>React</h4>
+                    <motion.img src={reactIcon} alt="React Icon" className="iconImage" whileHover={{ rotate: 360, transition: { repeat: Infinity, repeatType: 'loop', duration: 5 } }}/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>Node.Js</h4>
+                    <motion.img src={nodeIcon} alt="Node.Js Icon" className="iconImage" whileHover={{ rotate: [0, 30, 90, 150, 210, 270, 330, 360], transition: { repeat: Infinity, repeatType: 'loop', duration: 6 } }}/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>MongoDB</h4>
+                    <img src={mongoIcon} alt="MongoDB Icon" className="iconImage"/>
+                </div>
+                <div className="flex stackIconSingleContainer lastItemAlign">
+                    <h4>Express.Js</h4>
+                    <img src={expressIcon} alt="Express.Js Icon" className="iconImage"/>
+                </div>
             </div>
 
             <h1 className="stackTitle">Front-End</h1>
             <div className="stackIconOuterContainer flex">
-                    
-                    <div className="flex stackIconSingleContainer">
-                        <h4>HTML 5</h4>
-                        <img src={htmlIcon} alt="HTML5 Icon" className="iconImage"/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer">
-                        <h4>CSS 3</h4>
-                        <img src={cssIcon} alt="CSS3 Icon" className="iconImage"/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer">
-                        <h4>JavaScript</h4>
-                        <img src={javaIcon} alt="JavaScript Icon" className="iconImage"/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer">
-                        <h4>Bootstrap</h4>
-                        <img src={bootStrapIcon} alt="BootStrap Icon" className="iconImage"/>
-                    </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>HTML 5</h4>
+                    <img src={htmlIcon} alt="HTML5 Icon" className="iconImage"/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>CSS 3</h4>
+                    <img src={cssIcon} alt="CSS3 Icon" className="iconImage"/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>JavaScript</h4>
+                    <img src={javaIcon} alt="JavaScript Icon" className="iconImage"/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>Bootstrap</h4>
+                    <img src={bootStrapIcon} alt="BootStrap Icon" className="iconImage"/>
+                </div>
             </div>
 
             <h1 className="stackTitle">Back-End</h1>
-            <div className="stackIconOuterContainer flex">
-                    
-                    <div className="flex stackIconSingleContainer test">
-                        <h4>mySql</h4>
-                        <img src={mySqlIcon} alt="mySql Icon" className="iconImage"/>
-                    </div>
-
-                    <div className="flex stackIconSingleContainer">
-                        <h4>Handlebars</h4>
-                        <img src={handlebarsIcon} alt="Handlebars Icon" className="iconImage"/>
-                    </div>
+            <div className="stackIconOuterContainer flex">  
+                <div className="flex stackIconSingleContainer test">
+                    <h4>mySql</h4>
+                    <img src={mySqlIcon} alt="mySql Icon" className="iconImage"/>
+                </div>
+                <div className="flex stackIconSingleContainer">
+                    <h4>Handlebars</h4>
+                    <img src={handlebarsIcon} alt="Handlebars Icon" className="iconImage"/>
+                </div>
             </div>
         </div>
         <Footer/>
