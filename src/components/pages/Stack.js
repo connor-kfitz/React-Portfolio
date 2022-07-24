8c3c4209213b8f6ee1562c5c55194cb68b2560d9
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-24 17:08:09 -0400

    Finished Stack Animations

--- src/components/pages/Stack.js
+++ src/components/pages/Stack.js
@@ -5,12 +5,12 @@ import reactIcon from "../../images/stack-images/react-logo.png";
 import nodeIcon from "../../images/stack-images/node-js.webp"
 import mongoIcon from "../../images/stack-images/mongo_db.webp"
 import expressIcon from "../../images/stack-images/express.png"
-import htmlIcon from "../../images/stack-images/html5.svg"
-import cssIcon from "../../images/stack-images/css3-logo.png"
+import htmlIcon from "../../images/stack-images/html5.png"
+import cssIcon from "../../images/stack-images/css.png"
 import javaIcon from "../../images/stack-images/JavaScript-logo.png"
 import bootStrapIcon from "../../images/stack-images/bootstrap5-logo.jpeg"
 import mySqlIcon from "../../images/stack-images/mySql-logo.png"
-import handlebarsIcon from "../../images/stack-images/handlebars-logo.png"
+import handlebarsIcon from "../../images/stack-images/handlebar.png"
 
 import { motion } from 'framer-motion'
 
@@ -34,11 +34,11 @@ function Stack() {
                     </div>
                     <div className="flex stackIconSingleContainer">
                         <h4>MongoDB</h4>
-                        <img src={mongoIcon} alt="MongoDB Icon" className="iconImage"/>
+                        <motion.img src={mongoIcon} alt="MongoDB Icon" className="iconImage" whileHover={{ rotate: [0, 45, -45], transition: { yoyo: Infinity, repeatType: 'loop', duration: 4 } }}/>
                     </div>
                     <div className="flex stackIconSingleContainer lastItemAlign">
                         <h4>Express.Js</h4>
-                        <img src={expressIcon} alt="Express.Js Icon" className="iconImage"/>
+                        <motion.img src={expressIcon} alt="Express.Js Icon" className="iconImage" whileHover={{ scale: 1.2, transition: {duration: 1, yoyo: Infinity} }}/>
                     </div>
                 </div>
 
@@ -46,19 +46,19 @@ function Stack() {
                 <div className="stackIconOuterContainer flex">
                     <div className="flex stackIconSingleContainer">
                         <h4>HTML 5</h4>
-                        <img src={htmlIcon} alt="HTML5 Icon" className="iconImage"/>
+                        <motion.img src={htmlIcon} alt="HTML5 Icon" className="iconImage" whileHover={{ x:[0, 15, -15, 0], transition: {duration: 2, yoyo: Infinity}}}/>
                     </div>
                     <div className="flex stackIconSingleContainer">
                         <h4>CSS 3</h4>
-                        <img src={cssIcon} alt="CSS3 Icon" className="iconImage"/>
+                        <motion.img src={cssIcon} alt="CSS3 Icon" className="iconImage" whileHover={{ y:[0, -10, 10, 0], transition: {duration: 2, yoyo: Infinity}}}/>
                     </div>
                     <div className="flex stackIconSingleContainer">
                         <h4>JavaScript</h4>
-                        <img src={javaIcon} alt="JavaScript Icon" className="iconImage"/>
+                        <motion.img src={javaIcon} alt="JavaScript Icon" className="iconImage" whileHover={{ rotate: [0, 90, 180, 270, 360], transition: { repeat: Infinity, repeatType: 'loop', duration: 6 } }}/>
                     </div>
                     <div className="flex stackIconSingleContainer">
                         <h4>Bootstrap</h4>
-                        <img src={bootStrapIcon} alt="BootStrap Icon" className="iconImage"/>
+                        <motion.img src={bootStrapIcon} alt="BootStrap Icon" className="iconImage" whileHover={{ x:[0, -10, 10, 10, -10, -10, 0], y:[0, -10, -10, 10, 10, -10, 0], transition: {duration: 3, repeatType: "loop", repeat: Infinity}}}/>
                     </div>
                 </div>
 
@@ -66,11 +66,11 @@ function Stack() {
                 <div className="stackIconOuterContainer flex">  
                     <div className="flex stackIconSingleContainer test">
                         <h4>mySql</h4>
-                        <img src={mySqlIcon} alt="mySql Icon" className="iconImage"/>
+                        <motion.img src={mySqlIcon} alt="mySql Icon" className="iconImage" whileHover={{ rotate:[0, 0, -10, 0, 0,], x:[0, -10, -10, -10, 20, 0], y:[0, -10, -10, 10, 10, 0], transition: {duration: 2, repeatType: "loop", repeat: Infinity} }}/>
                     </div>
                     <div className="flex stackIconSingleContainer">
                         <h4>Handlebars</h4>
-                        <img src={handlebarsIcon} alt="Handlebars Icon" className="iconImage"/>
+                        <motion.img src={handlebarsIcon} alt="Handlebars Icon" className="iconImage"whileHover={{ x:[0, -10, 0, 10, 0, 10, 0, -10, 0], y:[0, -10, 0, -10, 0, 10, 0, 10, 0], transition: {duration: 7, repeatType: "loop", repeat: Infinity} }}/>
                     </div>
                 </div>
             </div>
